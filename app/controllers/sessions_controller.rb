d172a9fa7d7fd41cdc962bf078b70703729ddd8c
Michael Daye <mgdaye@gmail.com>
2018-05-14 01:57:12 -0400

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      flash[:success] = "Welcome back #{user.name}."
+    elsif !user
+      flash.now[:danger] = 'Email not found'
+    render 'new'
+    elsif !user.authenticate(params[:session][:password])
+      flash.now[:danger] = 'Incorrect password'
+    render 'new'
+    else
+      flash.now[:danger] = 'Unknown error occurred'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end

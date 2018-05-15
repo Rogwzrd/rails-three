595341b6a51a7809b186db6664a50a8a081b81fa
Michael Daye <mgdaye@gmail.com>
2018-05-15 07:58:43 -0400

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
-      flash[:success] = "Welcome back #{user.name}."
-    elsif !user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
+      flash[:success] = "Welcome back #{@user.name}."
+    elsif !@user
       flash.now[:danger] = 'Email not found'
     render 'new'
-    elsif !user.authenticate(params[:session][:password])
+    elsif !@user.authenticate(params[:session][:password])
       flash.now[:danger] = 'Incorrect password'
     render 'new'
     else
@@ -21,7 +22,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
